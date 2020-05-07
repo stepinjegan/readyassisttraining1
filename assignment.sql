51608d1f504b1aee944b6dd12e1b0c68da335563
NaGoo <64887925+NaGoo@users.noreply.github.com>
2020-05-07 15:12:01 +0530

    Update assignment.sql
    Adding details..

--- assignment.sql
+++ assignment.sql
@@ -12,3 +12,7 @@ pincode int not null,
 contact_person varchar(90),
 contact_number int not null
 );
+insert into address
+(Id,address_line1,address_line2,city,district,state,pincode,contact _person,contact _number);
+Values('1','st.marys street','thalavaipuram','nagercoil','kanyakumari','tamilnadu','629004','nagarajan','8610099836');
+Select *from address; 
